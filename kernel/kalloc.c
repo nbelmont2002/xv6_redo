15d93781cc061887e6e745d9cbfb8fe9262059eb
Nelly Belmont <mnellyb101@outlook.com>
2024-12-13 12:30:36 -0800

    lock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,22 +21,42 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  //initialize all the CPU locks
+  for (int cpu = 0; cpu < NCPU; cpu++){
+    initlock(&kmem[cpu].lock, "kmem");
+  }
+  
   freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmem[cpu].lock);
+
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    //code from kfree modified to work for only the CPU calling kinint
+    memset(p, 1, PGSIZE);
+
+    struct run *r;
+    r = (struct run*)p;
+    r->next = kmem[cpu].freelist;
+    kmem[cpu].freelist = r;
+  }
+
+  release(&kmem[cpu].lock);
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -53,13 +73,16 @@ kfree(void *pa)
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
-
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +92,42 @@ void *
 kalloc(void)
 {
   struct run *r;
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+  acquire(&kmem[cpu].lock);
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  r = kmem[cpu].freelist;
+  if (r)
+    kmem[cpu].freelist = r->next;
+  else
+  {
+    for (int i = 0; i < NCPU; i++)
+    {
+      //if we are checking ourselfs we skip this iteration else we
+      //get a lock on the CPU we're checking
+      if (i == cpu)
+        continue;
+      acquire(&kmem[i].lock);
+      //get the CPU's next page
+      r = kmem[i].freelist;
+      //if the page is free, we 'steal' it
+      if (r)
+      {	
+        kmem[i].freelist = r->next;
+        //we also release the lock on the CPU
+        release(&kmem[i].lock);
+        break;
+      }else{
+        //if we didnt get a blank page we release and keep going through the for loop
+        release(&kmem[i].lock);
+      }
+    }
+  }
+  //release CPU lock
+  release(&kmem[cpu].lock);
+ //fill the page
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
